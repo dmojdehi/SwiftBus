b446a123f9c0d5647f58332f0982add2e8113d5b
David <dmojdehi@gmail.com>
2017-11-21 14:59:12 -0800

    added tvOS example target (compiles, but does nothing)

---
+++ Example/SwiftBus_TV_Example/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SwiftBus_TV_Example
+//
+//  Created by David Mojdehi on 11/21/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import SwiftBus
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.agencyListTouched()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func agencyListTouched() {
+        SwiftBus.shared.transitAgencies() { result in
+            switch result {
+            case let .success(agencies):
+                let agenciesString = "Number of agencies loaded: \(agencies.count)"
+                let agencyNamesString = agencies.map({_, agency in "\(agency.agencyTitle)"})
+                
+                print("\n-----")
+                print(agenciesString)
+                print(agencyNamesString)
+                
+                //self.showAlertControllerWithTitle(agenciesString, message: "\(agencyNamesString)")
+            case let .error(error):
+                //self.showAlertControllerWithTitle("Error", message: error.localizedDescription)
+                break
+            }
+        }
+    }
+
+}
+
