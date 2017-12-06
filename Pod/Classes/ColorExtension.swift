d8b71e5a7e1448c02fdccfccd9923e149bcea555
David <dmojdehi@gmail.com>
2017-12-06 11:44:07 -0800

    don’t require ‘#’ prefix in color field (e.g. SF Muni feed doesn’t have them)

--- Pod/Classes/ColorExtension.swift
+++ Pod/Classes/ColorExtension.swift
@@ -30,39 +30,38 @@ private func parseRGBAString(_ rgba: String) -> (red:CGFloat, green:CGFloat, blu
     var blue:  CGFloat = 0.0
     var alpha: CGFloat = 1.0
     
-    if rgba.hasPrefix("#") {
-        let index   = rgba.index(rgba.startIndex, offsetBy: 1)
-        let hex     = String(rgba[index...])
-        let scanner = Scanner(string: hex)
-        var hexValue: CUnsignedLongLong = 0
-        if scanner.scanHexInt64(&hexValue) {
-            switch (hex.count) {
-            case 3:
-                red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
-                green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
-                blue  = CGFloat(hexValue & 0x00F)              / 15.0
-            case 4:
-                red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
-                green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
-                blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
-                alpha = CGFloat(hexValue & 0x000F)             / 15.0
-            case 6:
-                red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
-                green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
-                blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
-            case 8:
-                red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
-                alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
-            default:
-                print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
-            }
-        } else {
-            print("Scan hex error")
+    // trim any leading '#'
+    let rgbaToParse = rgba.trimmingCharacters(in: CharacterSet(charactersIn: "#"))
+    
+    let index   = rgbaToParse.index(rgba.startIndex, offsetBy: 0)
+    let hex     = String(rgbaToParse[index...])
+    let scanner = Scanner(string: hex)
+    var hexValue: CUnsignedLongLong = 0
+    if scanner.scanHexInt64(&hexValue) {
+        switch (hex.count) {
+        case 3:
+            red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+            green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+            blue  = CGFloat(hexValue & 0x00F)              / 15.0
+        case 4:
+            red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+            green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+            blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+            alpha = CGFloat(hexValue & 0x000F)             / 15.0
+        case 6:
+            red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+            green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+            blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+        case 8:
+            red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+            green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+            blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+            alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+        default:
+            print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
         }
     } else {
-        print("Invalid RGB string, missing '#' as prefix")
+        print("Scan hex error")
     }
     
     return (red, green, blue, alpha)
